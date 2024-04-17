77cc953160d694a5767c0c1e7df749aab92e1f7c
DC20-dev <dany2798@gmail.com>
2024-04-17 15:49:44 +0200

    added file_helper namespace to implement file helpers

---
+++ engine/core/file_helper.cpp
@@ -0,0 +1,32 @@
+#include "file_helper.h"
+
+#include <fstream>
+
+
+namespace core::file_helper
+{
+	std::expected<filedata, file_error> load_file(const std::filesystem::path& filepath)
+	{
+		std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+		if (!file)
+		{
+			return std::unexpected(file_error::file_not_found);
+		}
+
+		size_t size = file.tellg();
+		file.seekg(0, std::ios::beg);
+		size -= file.tellg();
+		if (size <= 0)
+		{
+			return std::unexpected(file_error::invalid_filesize);
+		}
+
+		filedata file_buffer(size);
+		if (!file.read(reinterpret_cast<char*>(file_buffer.data()), file_buffer.size()))
+		{
+			return std::unexpected(file_error::read_error);
+		}
+
+		return file_buffer;
+	}
+}
---
+++ engine/core/file_helper.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "bomb_engine.h"
+
+#include <vector>
+#include <filesystem>
+#include <expected>
+
+namespace core::file_helper
+{
+	enum class file_error
+	{
+		file_not_found = 0,
+		invalid_filesize,
+		read_error,
+	};
+
+	using filedata = std::vector<std::byte>;
+
+	BOMB_ENGINE_API std::expected<filedata, file_error> load_file(const std::filesystem::path& filepath);
+}
