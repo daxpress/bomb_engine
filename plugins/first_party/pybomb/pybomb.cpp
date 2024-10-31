f5266f1fc2afd1e1aae518340c2a37c74b8d1417
DC20-dev <dany2798@gmail.com>
2024-10-31 15:14:49 +0100

    * custom command to generate stub files for pybomb post build to enable code completion (stubgen required) * added comments

--- plugins/first_party/pybomb/pybomb.cpp
+++ plugins/first_party/pybomb/pybomb.cpp
@@ -5,5 +5,6 @@ namespace py = pybind11;
 // Declared by generated file!!!
 void init_pybomb(py::module& m)
 {
+    // testing module functionality
     m.def("print", [](){Log(LogTempCategory, LogSeverity::Log, "python print using module");});
 }
